28bd1755705fa4df764e3efe60bf91371f85dbc7
Jose Vitor <vitosantanatrabalho@hotmail.com>
2023-06-20 23:27:10 -0300

    api para testes

--- src/test/kotlin/domain/ClienteTeste.kt
+++ src/test/kotlin/domain/ClienteTeste.kt
@@ -14,13 +14,13 @@ class ClienteTeste {
     @DisplayName ("Quando criar o cliente James bond, Então seu risco deve ser calculado corretamente ")
     fun verficaCalculoDeRisco() {
         val clienteEsperado = Cliente(
-            nome = "James Bond",
+            nome = "Vitu",
             idade = 35,
             id = 1007,
             risco = -65
         )
         val clienteObtido = Cliente(
-            nome = "James Bond",
+            nome = "Vitu",
             idade = 35,
             id = 1007)
         assertThat(clienteEsperado.risco).isEqualTo(clienteObtido.calcularRisco())
